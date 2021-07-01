4b567befbd38772858be54bbd16704c43417414c
Toni Rib <toni.rib@gusto.com>
2021-07-01 12:03:34 -0600

    Add comments

--- scripts/upgrade_kernel.sh
+++ scripts/upgrade_kernel.sh
@@ -7,4 +7,7 @@ set -o errexit
 sudo apt-get update -y
 sudo apt-get -y dist-upgrade
 
+# The latest version of the amazon eks ami allows for the user to choose a kernel version
+# We are not supporting this capability, so that code has been left out of this file
+
 sudo reboot
